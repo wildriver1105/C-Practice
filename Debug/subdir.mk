7928d1341481e7173caf80d0f8fc9a1b4f18ed48
Hyun Lee <wildriver1105@gmail.com>
2026-03-26 23:03:48 +0900

    What is this

---
+++ Debug/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main.c 
+
+OBJS += \
+./main.o 
+
+C_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o %.su %.cyclo: ../%.c subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -c -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean--2e-
+
+clean--2e-:
+	-$(RM) ./main.cyclo ./main.d ./main.o ./main.su
+
+.PHONY: clean--2e-
+
