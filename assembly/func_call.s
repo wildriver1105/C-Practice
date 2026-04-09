349e1d120bfb169d3fd84ca572be0207fd1355dd
Hyun Lee <wildriver1105@gmail.com>
2026-04-09 22:40:37 +0900

    New assembly

---
+++ assembly/func_call.s
@@ -0,0 +1,44 @@
+	.build_version macos, 26, 0	sdk_version 26, 4
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_add                            ; -- Begin function add
+	.p2align	2
+_add:                                   ; @add
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	w0, [sp, #12]
+	str	w1, [sp, #8]
+	ldr	w8, [sp, #12]
+	ldr	w9, [sp, #8]
+	add	w0, w8, w9
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #32
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #0                          ; =0x0
+	str	w8, [sp, #4]                    ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	mov	w0, #10                         ; =0xa
+	mov	w1, #20                         ; =0x14
+	bl	_add
+	mov	x8, x0
+	ldr	w0, [sp, #4]                    ; 4-byte Folded Reload
+	str	w8, [sp, #8]
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	add	sp, sp, #32
+	ret
+	.cfi_endproc
+                                        ; -- End function
+.subsections_via_symbols
