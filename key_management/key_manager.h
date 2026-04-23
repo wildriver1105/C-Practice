19b8e81d5210157e132a690af1891a8b8b18bc7c
Hyun Lee <wildriver1105@gmail.com>
2026-04-23 23:45:28 +0900

    Header file written

---
+++ key_management/key_manager.h
@@ -0,0 +1,23 @@
+#ifndef KEY_MANAGER_H
+#define KEY_MANAGER_H
+
+#include <stddef.h>
+
+typedef struct {
+    int id;
+    char label[32];
+    char *secret_key;
+} CryptoKey;
+
+typedef struct {
+    CryptoKey * keys;
+    int count;
+    int capacity;
+} KeyStorage;
+
+KeyStorage* init_storage(int initial_capacity);
+int add_key(KeyStorage *storage, int id, const char *label, const char *secret);
+void print_all_keys(const KeyStorage *storage);
+void free_storage(KeyStorage *storage);
+
+#endif
