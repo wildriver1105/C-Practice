6652fc60aacb8a89af76930fa36824492853a9b9
Hyun Lee <wildriver1105@gmail.com>
2026-04-24 00:01:17 +0900

    main written

---
+++ key_management/main.c
@@ -0,0 +1,14 @@
+#include "key_manager.h"
+
+int main() {
+    KeyStorage *my_storage = init_storage(2);
+
+    add_key(my_storage, 101, "AWS_ROOT", "AKIA-XXXX-YYYY");
+    add_key(my_storage, 102, "GITHUB_TOKEN", "ghp_secure_token_123");
+
+    print_all_keys(my_storage);
+
+    free_storage(my_storage);
+
+    return 0;
+}
