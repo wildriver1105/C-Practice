41700bcfb192008b597578eac81f274dc74fc68d
Hyun Lee <wildriver1105@gmail.com>
2026-05-09 23:44:37 +0900

    Type error fixed

--- memory_management/mm.h
+++ memory_management/mm.h
@@ -2,6 +2,13 @@
 // #define MM_MAX_STRUCT_NAME (SYSTEM_PAGE_SIZE - sizeof(vm_page_for_families_t *)) / sizeof(vm_page_family_t)
 #define MM_MAX_STRUCT_NAME 64
 
+typedef enum {
+    MM_FALSE,
+    MM_TRUE
+} vm_bool_t;
+
+struct vm_page_family_;
+
 typedef struct vm_page_family_ {
     char struct_name[MM_MAX_STRUCT_NAME];
     uint32_t struct_size;
