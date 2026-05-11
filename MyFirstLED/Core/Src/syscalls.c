44d723e568f06f2b46cd80a38ad9a67833a17d04
Hyun Lee <wildriver1105@gmail.com>
2026-05-11 22:42:53 +0900

    Debugging code added

--- MyFirstLED/Core/Src/syscalls.c
+++ MyFirstLED/Core/Src/syscalls.c
@@ -20,6 +20,12 @@
  ******************************************************************************
  */
 
+#define DEMCR        			*((volatile uint32_t*) 0xE000EDFCU )
+
+/* ITM register addresses */
+#define ITM_STIMULUS_PORT0   	*((volatile uint32_t*) 0xE0000000 )
+#define ITM_TRACE_EN          	*((volatile uint32_t*) 0xE0000E00 )
+
 /* Includes */
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -39,6 +45,21 @@ extern int __io_getchar(void) __attribute__((weak));
 char *__env[1] = { 0 };
 char **environ = __env;
 
+void ITM_SendChar(uint8_t ch)
+{
+
+	//Enable TRCENA
+	DEMCR |= ( 1 << 24);
+
+	//enable stimulus port 0
+	ITM_TRACE_EN |= ( 1 << 0);
+
+	// read FIFO status in bit [0]:
+	while(!(ITM_STIMULUS_PORT0 & 1));
+
+	//Write to ITM stimulus port0
+	ITM_STIMULUS_PORT0 = ch;
+}
 
 /* Functions */
 void initialise_monitor_handles()
@@ -84,7 +105,8 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
 
   for (DataIdx = 0; DataIdx < len; DataIdx++)
   {
-    __io_putchar(*ptr++);
+//    __io_putchar(*ptr++);
+	  ITM_SendChar(*ptr++);
   }
   return len;
 }
